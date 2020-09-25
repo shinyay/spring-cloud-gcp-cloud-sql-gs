26fd52b1050d9fa8f38b264cc7eb0ed5666ff060
shinyay <shinya.com@gmail.com>
2020-09-25 13:04:20 +0900

    Add spring-boot-starter-jdbc

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.cloud:spring-cloud-gcp-starter")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	runtimeOnly("mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
