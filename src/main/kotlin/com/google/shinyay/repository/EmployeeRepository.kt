898eee5c0a89d612425316a61740d0c884f7653b
shinyay <shinya.com@gmail.com>
2020-09-30 17:38:24 +0900

    Add query Employee Table in Cloud SQL with JDBC Template

--- src/main/kotlin/com/google/shinyay/repository/EmployeeRepository.kt
+++ src/main/kotlin/com/google/shinyay/repository/EmployeeRepository.kt
@@ -1,7 +1,13 @@
 package com.google.shinyay.repository
 
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Repository
 
+
 @Repository
-class EmployeeRepository {
+class EmployeeRepository(val jdbcTemplate: JdbcTemplate) {
+
+    fun getEmployees(): List<String>? = jdbcTemplate.queryForList("SELECT * FROM employee")
+            .map { m: Map<String?, Any?> -> m.values.toString() }
+            .toList()
 }
