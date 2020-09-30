c1e2d994f1326fd5fa317c54172af63e6e823602
shinyay <shinya.com@gmail.com>
2020-09-30 19:15:24 +0900

    Add controller for calling repository class

--- src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
+++ src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
@@ -1,8 +1,12 @@
 package com.google.shinyay.controller
 
-import org.springframework.jdbc.core.JdbcTemplate
+import com.google.shinyay.repository.EmployeeRepository
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class EmployeeController(val jdbcTemplate: JdbcTemplate) {
+class EmployeeController(val repository: EmployeeRepository) {
+
+    @GetMapping("/employees")
+    fun findAllEmployees() = repository.getEmployees()
 }
