b6c7ef85ea978d9c50e95b246010acabe0c81b30
shinyay <shinya.com@gmail.com>
2020-10-01 22:36:48 +0900

    Add creating department table

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS department;
+CREATE TABLE department (
+                            department_id decimal(2,0) DEFAULT NULL,
+                            department_name varchar(14) DEFAULT NULL
+);
