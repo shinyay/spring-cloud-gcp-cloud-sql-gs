671b82adc31281e9c71b4f4155dccaa9f0b9f38b
shinyay <shinya.com@gmail.com>
2020-10-02 13:14:04 +0900

    Add JpaRepository implementation interface to operate Department entity

---
+++ src/main/kotlin/com/google/shinyay/repository/DepartmentRepository.kt
@@ -0,0 +1,9 @@
+package com.google.shinyay.repository
+
+import com.google.shinyay.entity.Department
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface DepartmentRepository : JpaRepository<Department, Int> {
+}
