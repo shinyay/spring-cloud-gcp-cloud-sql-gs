dd8edca16828b53514efca802e8f2467251ee6ed
shinyay <shinya.com@gmail.com>
2020-10-02 11:56:05 +0900

    Add Table annotation to specify table name

--- src/main/kotlin/com/google/shinyay/entity/Department.kt
+++ src/main/kotlin/com/google/shinyay/entity/Department.kt
@@ -3,5 +3,6 @@ package com.google.shinyay.entity
 import javax.persistence.*
 
 @Entity
+@Table(name = "department")
 data class Department(val department_id: Long,
                       val name: String)
