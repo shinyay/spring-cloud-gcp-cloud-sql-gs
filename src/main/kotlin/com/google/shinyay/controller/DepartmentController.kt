3fee7ca3129371a416f3468b6aa8639c349b9230
shinyay <shinya.com@gmail.com>
2020-10-02 14:40:37 +0900

    Add controller for Department

--- src/main/kotlin/com/google/shinyay/controller/DepartmentController.kt
+++ src/main/kotlin/com/google/shinyay/controller/DepartmentController.kt
@@ -1,8 +1,12 @@
 package com.google.shinyay.controller
 
 import com.google.shinyay.service.DepartmentService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class DepartmentController(val service: DepartmentService) {
+
+    @GetMapping("/departments")
+    fun getDepartments() = service.getDepartmentList()
 }
