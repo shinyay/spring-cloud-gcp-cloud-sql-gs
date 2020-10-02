66946798b53a51b16653f75eadfcdae845e7cf27
shinyay <shinya.com@gmail.com>
2020-10-02 14:10:50 +0900

    Add service function to get department all info

--- src/main/kotlin/com/google/shinyay/service/DepartmentService.kt
+++ src/main/kotlin/com/google/shinyay/service/DepartmentService.kt
@@ -1,5 +1,6 @@
 package com.google.shinyay.service
 
+import com.google.shinyay.entity.Department
 import com.google.shinyay.repository.DepartmentRepository
 import org.springframework.stereotype.Service
 
@@ -7,4 +8,6 @@ import org.springframework.stereotype.Service
 class DepartmentService(val repository: DepartmentRepository) {
 
     fun getDepartmentById(departmentId: Int) = repository.findById(departmentId)
+
+    fun getDepartmentList(): List<Department> = repository.findAll()
 }
