3734f87ad51ad779abb8ab9fe3efa054426aadac
shinyay <shinya.com@gmail.com>
2020-10-02 14:08:07 +0900

    Add service for department repository to retrielve department info

--- src/main/kotlin/com/google/shinyay/service/DepartmentService.kt
+++ src/main/kotlin/com/google/shinyay/service/DepartmentService.kt
@@ -1,7 +1,10 @@
 package com.google.shinyay.service
 
+import com.google.shinyay.repository.DepartmentRepository
 import org.springframework.stereotype.Service
 
 @Service
-class DepartmentService {
+class DepartmentService(val repository: DepartmentRepository) {
+
+    fun getDepartmentById(departmentId: Int) = repository.findById(departmentId)
 }
