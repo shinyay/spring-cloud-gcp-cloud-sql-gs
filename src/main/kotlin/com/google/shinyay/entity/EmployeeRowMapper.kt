f551715b6e2e63e8376d739ff92d1bf2da6064ec
shinyay <shinya.com@gmail.com>
2020-10-03 21:36:23 +0900

    Add default constructor for Employee entity

--- src/main/kotlin/com/google/shinyay/entity/EmployeeRowMapper.kt
+++ src/main/kotlin/com/google/shinyay/entity/EmployeeRowMapper.kt
@@ -3,7 +3,7 @@ package com.google.shinyay.entity
 import org.springframework.jdbc.core.RowMapper
 import java.sql.ResultSet
 
-class EmployeeRowMapper : RowMapper<Employee> {
+class EmployeeRowMapper(val employee: Employee) : RowMapper<Employee> {
     override fun mapRow(rs: ResultSet, rowNum: Int): Employee? {
         TODO("Not yet implemented")
     }
