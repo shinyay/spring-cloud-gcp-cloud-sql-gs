7c13bdc7e54b70114315d9e56b56a28bafd63f66
shinyay <shinya.com@gmail.com>
2020-10-04 10:24:30 +0900

    Add function to retrieve data with RowMapper

--- src/main/kotlin/com/google/shinyay/repository/EmployeeRepository.kt
+++ src/main/kotlin/com/google/shinyay/repository/EmployeeRepository.kt
@@ -1,5 +1,6 @@
 package com.google.shinyay.repository
 
+import com.google.shinyay.entity.EmployeeRowMapper
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Repository
 
@@ -11,5 +12,11 @@ class EmployeeRepository(val jdbcTemplate: JdbcTemplate) {
             .map { m: Map<String?, Any?> -> m.values.toString() }
             .toList()
 
-    fun getEmployeeByName(){}
+    fun getEmployeeByName(name: String){
+        jdbcTemplate.queryForObject(
+                "SELECT * FROM EMPLOYEE WHERE employee_name = ?",
+                arrayOf<Any>(name),
+                EmployeeRowMapper()
+        )
+    }
 }
