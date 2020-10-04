eb79ff8db7900c8acf9a6e5208511edae2bb76d2
shinyay <shinya.com@gmail.com>
2020-10-04 11:02:31 +0900

    Add Path Variable to specify employee name by API

--- src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
+++ src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
@@ -2,6 +2,7 @@ package com.google.shinyay.controller
 
 import com.google.shinyay.repository.EmployeeRepository
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -9,4 +10,7 @@ class EmployeeController(val repository: EmployeeRepository) {
 
     @GetMapping("/employees")
     fun findAllEmployees() = repository.getEmployees()
+
+    @GetMapping("/employee/{name}")
+    fun findEmployeeByName(@PathVariable("name") name: String) = repository.getEmployeeByName(name)
 }
