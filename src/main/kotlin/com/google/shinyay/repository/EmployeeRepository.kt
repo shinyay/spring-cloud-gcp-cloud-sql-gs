7e20020c105f7b28a6b523f1f8b4a842b533a90b
shinyay <shinya.com@gmail.com>
2020-10-04 10:52:50 +0900

    Modify table name as lower case

--- src/main/kotlin/com/google/shinyay/repository/EmployeeRepository.kt
+++ src/main/kotlin/com/google/shinyay/repository/EmployeeRepository.kt
@@ -1,8 +1,11 @@
 package com.google.shinyay.repository
 
+import com.google.shinyay.entity.Employee
 import com.google.shinyay.entity.EmployeeRowMapper
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.queryForObject
 import org.springframework.stereotype.Repository
+import java.sql.ResultSet
 
 
 @Repository
@@ -12,9 +15,9 @@ class EmployeeRepository(val jdbcTemplate: JdbcTemplate) {
             .map { m: Map<String?, Any?> -> m.values.toString() }
             .toList()
 
-    fun getEmployeeByName(name: String){
+    fun getEmployeeByName(name: String): Employee? {
         jdbcTemplate.queryForObject(
-                "SELECT * FROM EMPLOYEE WHERE employee_name = ?",
+                "SELECT * FROM employee WHERE employee_name = ?",
                 arrayOf<Any>(name),
                 EmployeeRowMapper()
         )
