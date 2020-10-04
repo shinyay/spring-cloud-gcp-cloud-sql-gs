2a3cad3ed2e685cca09b3d18034cac85021db244
shinyay <shinya.com@gmail.com>
2020-10-04 09:05:52 +0900

    Add RowMapper for Employee entity

--- src/main/kotlin/com/google/shinyay/entity/EmployeeRowMapper.kt
+++ src/main/kotlin/com/google/shinyay/entity/EmployeeRowMapper.kt
@@ -3,8 +3,13 @@ package com.google.shinyay.entity
 import org.springframework.jdbc.core.RowMapper
 import java.sql.ResultSet
 
-class EmployeeRowMapper(val employee: Employee) : RowMapper<Employee> {
+class EmployeeRowMapper : RowMapper<Employee> {
     override fun mapRow(rs: ResultSet, rowNum: Int): Employee? {
-        TODO("Not yet implemented")
+        return Employee(
+                rs.getLong("employee_id"),
+                rs.getString("employee_name"),
+                rs.getString("role"),
+                rs.getLong("department_id")
+        )
     }
 }
