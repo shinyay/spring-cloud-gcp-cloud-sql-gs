d8d34bf9b450fa3a222e555e2d89aec6058aa5f8
shinyay <shinya.com@gmail.com>
2020-10-05 13:25:38 +0900

    Add Custom Query with Standard SQL not JPQL

--- src/main/kotlin/com/google/shinyay/repository/LocationRepository.kt
+++ src/main/kotlin/com/google/shinyay/repository/LocationRepository.kt
@@ -1,8 +1,13 @@
 package com.google.shinyay.repository
 
 import com.google.shinyay.entity.Location
+import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface LocationRepository : CrudRepository<Location, Long>
+interface LocationRepository : CrudRepository<Location, Long> {
+
+    @Query("SELECT COUNT(*) FROM location")
+    fun countAll()
+}
