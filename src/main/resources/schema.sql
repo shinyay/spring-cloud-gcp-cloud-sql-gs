4a4f57d07b943b44b0a2a6e55be29a4184fa1eb5
shinyay <shinya.com@gmail.com>
2020-10-05 12:55:52 +0900

    Add Create SQL for Location table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,10 @@ CREATE TABLE department (
                             department_id decimal(2,0) DEFAULT NULL,
                             department_name varchar(14) DEFAULT NULL
 );
+
+DROP TABLE IF EXISTS location;
+CREATE TABLE location (
+                            location_id decimal(2,0) DEFAULT NULL,
+                            city varchar(64) DEFAULT NULL,
+                            postal_code varchar(8) DEFAULT NULL
+);
