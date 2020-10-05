8dc42ec95fe98e15147a113a6df4c2ebbdffe703
shinyay <shinya.com@gmail.com>
2020-10-05 13:41:07 +0900

    Add constructor injection

--- src/main/kotlin/com/google/shinyay/controller/LocationController.kt
+++ src/main/kotlin/com/google/shinyay/controller/LocationController.kt
@@ -1,7 +1,11 @@
 package com.google.shinyay.controller
 
+import com.google.shinyay.service.LocationService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class LocationController {
+class LocationController(val service: LocationService) {
+
+
 }
