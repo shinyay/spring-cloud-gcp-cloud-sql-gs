ce66d0997fbef72e30ea5bd0de4cf49d9891ffdf
shinyay <shinya.com@gmail.com>
2020-10-05 16:50:40 +0900

    Delete Location domain due to parameter 0 of constructor error at the same time with Spring Data JPA

--- src/main/kotlin/com/google/shinyay/entity/Location.kt
+++
@@ -1,7 +0,0 @@
-package com.google.shinyay.entity
-
-import org.springframework.data.annotation.Id
-
-data class Location(@Id val id: Long,
-                    val city: String,
-                    val postal_code: String)
