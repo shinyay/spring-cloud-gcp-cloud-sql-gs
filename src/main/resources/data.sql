34a975b0c3ac2b2bd95a2fb7ff5a59c7fe01827f
shinyay <shinya.com@gmail.com>
2020-10-05 12:56:26 +0900

    Add data into location table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,9 @@ INSERT INTO department VALUES ('10','ACCOUNTING');
 INSERT INTO department VALUES ('20','RESEARCH');
 INSERT INTO department VALUES ('30','SALES');
 INSERT INTO department VALUES ('40','OPERATIONS');
+
+INSERT INTO location VALUES ('91','札幌市北区','0010000');
+INSERT INTO location VALUES ('92','札幌市北区北十条西','0010010');
+INSERT INTO location VALUES ('93','札幌市北区北十一条西','0010011');
+INSERT INTO location VALUES ('94','札幌市北区北十二条西','0010012');
+INSERT INTO location VALUES ('95','札幌市北区北十三条西','0010013');
